db7fd666c3a49f3adcc7b9906241baf5a945e234
Alex Merced <willcodeforfood@alexmerced.com>
2020-10-15 14:07:10 -0400

    first commit

---
+++ src/pages/home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Home = (props) => {
+
+
+
+  return (
+    <div>
+        <h1>Welcome</h1>
+        <p>Please signup then login to view your user dashboard!</p>
+    </div>
+  );
+};
+
+export default Home;
